5f548f3268769276e549cd8cd18d0b45fd819d7c
MrTryHard <alex.cs00@yahoo.ca>
2016-07-30 22:13:21 -0400

    Added basic user view (no functionnality). See #9.

---
+++ src/MyTryHard/ViewModels/AdminUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyTryHard.Models;
+using MyTryHard.ViewModels.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTryHard.ViewModels
+{
+    public class AdminUserViewModel
+    {
+        public RegisterViewModel UserVM { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
