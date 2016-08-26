ed295b7674417fb96989e6fd02d4b92cee7a1404
MrTryHard <alex.cs00@yahoo.ca>
2016-08-26 17:52:51 -0400

    Added pertinent info in entries. See issue #15.

--- sql/create_scripts/table_tracker_entry.sql
+++ sql/create_scripts/table_tracker_entry.sql
@@ -1,7 +1,11 @@
+-- Distance is stored in meters. 
 CREATE TABLE IF NOT EXISTS "dbo"."TrackerEntry" (
   "TrackerEntryID" SERIAL NOT NULL, 
   "TrackedSportID" INT NOT NULL,
   "UserID" VARCHAR(36) NOT NULL, 
+  "Distance" INT NOT NULL, 
+  "DateTimeStart" TIMESTAMP NOT NULL,
+  "DateTimeEnd" TIMESTAMP NOT NULL,
   CONSTRAINT "pk_TrackerEntryID" PRIMARY KEY ("TrackerEntryID"),
   CONSTRAINT "fk_TrackerEntryTrackedSportID" FOREIGN KEY ("TrackedSportID")
     REFERENCES "dbo"."TrackedSports" ("TrackedSportID"),
